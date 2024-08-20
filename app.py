57d8b5d570c2c25fa5341b1aa776d817e365942e
Sitam Meur <103279526+sitamgithub-MSIT@users.noreply.github.com>
2024-08-20 22:11:55 +0000

    main gradio app modified accordingly

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 # Importing the requirements
+import warnings
+warnings.filterwarnings("ignore")
+
 import gradio as gr
-from model import answer_question
+from src.app.response import describe_image
 
 
 # Image and text inputs for the interface
 image = gr.Image(type="pil", label="Image")
-question = gr.Textbox(label="Question")
+question = gr.Textbox(label="Question", placeholder="Enter your question here")
 
 # Output for the interface
 answer = gr.Textbox(label="Predicted answer", show_label=True, show_copy_button=True)
@@ -19,20 +22,21 @@ examples = [
 
 # Title, description, and article for the interface
 title = "Visual Question Answering"
-description = "Gradio Demo for the MiniCPM Llama3 Vision Language Understanding and Generation model. This model can answer questions about images in natural language. To use it, simply upload your image and type a question and click 'submit', or click one of the examples to load them. Read more at the links below."
-article = "<p style='text-align: center'><a href='https://github.com/OpenBMB/MiniCPM-V' target='_blank'>Model GitHub Repo</a> | <a href='https://huggingface.co/openbmb/MiniCPM-Llama3-V-2_5' target='_blank'>Model Page</a></p>"
+description = "Gradio Demo for the MiniCPM-V 2.6 Vision Language Understanding and Generation model. This model can answer questions about images in natural language. To use it, simply upload your image, type a question, and click 'submit', or click one of the examples to load them. Read more at the links below."
+article = "<p style='text-align: center'><a href='https://github.com/OpenBMB/MiniCPM-V' target='_blank'>Model GitHub Repo</a> | <a href='https://huggingface.co/openbmb/MiniCPM-V-2_6' target='_blank'>Model Page</a></p>"
 
 
 # Launch the interface
 interface = gr.Interface(
-    fn=answer_question,
+    fn=describe_image,
     inputs=[image, question],
     outputs=answer,
     examples=examples,
+    cache_examples="lazy",
     title=title,
     description=description,
     article=article,
-    theme="Soft",
+    theme="JohnSmith9982/small_and_pretty",
     allow_flagging="never",
 )
 interface.launch(debug=False)
